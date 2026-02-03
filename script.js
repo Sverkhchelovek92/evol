cb7b35db23184d173467d114a6d34cae62a1ddf3
Miroslav Gurnyak <mirmiroslav@gmail.com>
2026-02-03 13:02:23 +0300

    audio load

--- script.js
+++ script.js
@@ -9,3 +9,27 @@ const progressContainer = document.querySelector('.progress-container')
 
 const currentTimeEl = document.getElementById('currentTime')
 const durationEl = document.getElementById('duration')
+
+// Audio element
+
+const audio = new Audio()
+audio.preload = 'metadata'
+
+fileInput.addEventListener('change', (e) => {
+  const file = e.target.files[0]
+  if (!file) return
+
+  trackInfo.textContent = file.name
+
+  const objectURL = URL.createObjectURL(file)
+
+  audio.src = objectURL
+
+  progress.style.width = '0%'
+  currentTimeEl.textContent = '0:00'
+  durationEl.textContent = '0:00'
+
+  playBtn.disabled = true
+  pauseBtn.style.display = 'none'
+  playBtn.style.display = 'inline-block'
+})
