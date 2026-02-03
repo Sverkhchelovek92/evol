294de16b42b1b16574b63dadb874e77ce18d0015
Miroslav Gurnyak <mirmiroslav@gmail.com>
2026-02-03 12:32:43 +0300

    style and constants

---
+++ script.js
@@ -0,0 +1,11 @@
+const fileInput = document.getElementById('fileInput')
+const trackInfo = document.querySelector('.track-info')
+
+const playBtn = document.getElementById('playBtn')
+const pauseBtn = document.getElementById('pauseBtn')
+
+const progress = document.querySelector('.progress')
+const progressContainer = document.querySelector('.progress-container')
+
+const currentTimeEl = document.getElementById('currentTime')
+const durationEl = document.getElementById('duration')
