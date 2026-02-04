86266fcdeecff76993237b5c075f8243407822ec
Miroslav Gurnyak <mirmiroslav@gmail.com>
2026-02-04 13:45:38 +0300

    format time func

--- script.js
+++ script.js
@@ -41,4 +41,9 @@ audio.addEventListener('loadedmetadata', () => {
   durationEl.textContent = formatTime(duration)
 })
 
-function formatTime(seconds) {}
+function formatTime(seconds) {
+  if (!seconds || isNaN(seconds)) return '0:00'
+  const min = Math.floor(seconds / 60)
+  const sec = Math.floor(seconds % 60)
+  return `${min}:${sec.toString().padStart(2, '0')}`
+}
